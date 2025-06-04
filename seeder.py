48180af94311b58ab59b7747eb319a552ca1a861
gmolins <guillermo.molins_mendez@mercedes-benz.com>
2025-06-04 19:30:59 +0200

    Update seeder

--- seeder.py
+++ seeder.py
@@ -5,11 +5,10 @@ from db.database import engine, create_db_and_tables, drop_db_and_tables
 from models.user import User
 from models.order import Order
 from models.status import Status
-from auth.hashing import hash_password  # Importamos la función para hashear contraseñas
+from auth.hashing import hash_password
 
 def seed_data(num_dummies=5):
     with Session(engine) as session:
-        # Crear usuarios
         try:
             users = []
             for i in range(num_dummies):
@@ -20,34 +19,29 @@ def seed_data(num_dummies=5):
         except Exception as e:
             print(f"Error creating users: {e}")
 
-        # Crear todos linkados a usuarios
+        try:
+            status1 = Status(name=f"Order Created", color=f"Yellow")
+            status2 = Status(name=f"Order Placed", color=f"Blue")
+            status3 = Status(name=f"Payment Complete", color=f"Green")
+            status4 = Status(name=f"Delivered", color=f"Purple")
+            status5 = Status(name=f"RMA", color=f"Red")
+            session.add_all([status1, status2, status3, status4, status5])
+            session.commit()
+        except Exception as e:
+            print(f"Error creating status: {e}")
+
         try:
             todos = []
             for j in range(num_dummies):
-                todos.append(Order(created_at=datetime.now(), user_id=j+1))
+                todos.append(Order(created_at=datetime.now(), user_id=j+1, status_id=1))
             session.add_all(todos)
             session.commit()
         except Exception as e:
             print(f"Error creating todos: {e}")
-
-        # Crear status
-        try:
-            status1 = Status(name=f"Order Created", color=f"Yellow")
-            status1 = Status(name=f"Order Placed", color=f"Blue")
-            status2 = Status(name=f"Payment Complete", color=f"Green")
-            status3 = Status(name=f"Delivered", color=f"Purple")
-            status3 = Status(name=f"RMA", color=f"Red")
-            session.add_all([status1, status2, status3])
-            session.commit()
-        except Exception as e:
-            print(f"Error creating status: {e}")
         
     print("Tables seeded successfully!")
 
 if __name__ == "__main__":
-    # Borrar la base de datos y las tablas existentes
     drop_db_and_tables() 
-    # Crear la base de datos y las tablas
     create_db_and_tables()
-    # Alimentar las tablas con datos dummy
     seed_data()
